fa9a06833740e42f76167e584db7ad26af8700c4
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-14 23:17:39 +0530

    Create BottomSheet Layout

---
+++ app/src/main/java/com/sudipta/bottomsheet/BottomSheetFragment.java
@@ -0,0 +1,42 @@
+package com.sudipta.bottomsheet;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+
+//extends ftagment to extends BottomsheetDialogFragment
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+
+
+    public BottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static BottomSheetFragment newInstance(String param1, String param2) {
+        BottomSheetFragment fragment = new BottomSheetFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+    }
+}
---
+++ app/src/main/java/com/sudipta/bottomsheet/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sudipta.bottomsheet;
+
+/**
+ * 1. add fragment BottomSheetfragment.
+ * 2. add background xml file in drawable.
+ * 3. add some style theme.
+ **/
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+                bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+            }
+        });
+    }
+}
